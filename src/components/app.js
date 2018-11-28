ff73c25d03131082b86a6f2b71d20d5b1855f914
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-11-28 07:15:22 +0530

    new codes

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import PlayGround from './PlayGround'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Change me</h1>
+        <PlayGround />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
 
-
-
-
-
 window.onload = () => {
   ReactDOM.render(<App/>, document.getElementById('main'));
 };
