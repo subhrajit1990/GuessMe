47b366efa8b0335528e8a3d21d16d7a2c3a8b273
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-12-02 16:11:41 +0530

    new files for api migration

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 const path = require('path')
 const express = require('express')
-var cors = require('cors');
-var whitelist = ['https://myapplicationdata.herokuapp.com/'];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-};
-
-
 module.exports = {
   app: function () {
     const app = express();
-    app.use(cors(corsOptions));
+    
     const indexPath = path.join(__dirname, 'index.html');
     const publicPath = express.static(path.join(__dirname, 'public'));
     app.use('/public', publicPath);   
