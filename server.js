abff29a70cad5b18ca56b031c00c085cfdf223ea
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-12-03 08:25:07 +0530

    dummy

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const express = require('express')
-var cors = require('cors')
+
 
 module.exports = {
   app: function () {
     var app = express();
-	app.use(cors());   
+	
     const indexPath = path.join(__dirname, 'index.html');
     const publicPath = express.static(path.join(__dirname, 'public'));
     app.use('/public',publicPath);   
