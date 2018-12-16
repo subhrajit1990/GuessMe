ba8681765aa8325f206d754f9d47a2395f0de2d1
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-12-16 09:21:36 +0530

    new changes for api call:Subhrajit

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ module.exports = {
   app: function () {
     var app = express();
 	
+	app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
     const indexPath = path.join(__dirname, 'index.html');
     const publicPath = express.static(path.join(__dirname, 'public'));
     app.use('/public',publicPath);   
-    app.get('/*', function (_, res) {res.sendFile(indexPath) });
+    app.get('/', function (_, res) {res.sendFile(indexPath) });
 
    return app;
   }
