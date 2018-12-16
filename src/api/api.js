0c12e13949bce37e7af271b2f1dac154bbd9436e
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-12-16 11:42:30 +0530

    new codes for API:Subhrajit

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,7 @@ export default function requestService(url, options = {}) {
     options,
   };
 
-  return fetch("https://myapplicationdata.herokuapp.com"+url,extraParameters)
+  return fetch("https://myapplicationdata.herokuapp.com/"+url,extraParameters)
     .then(res => callBackFunction(res))
     .catch(error => console.log(error))
         
