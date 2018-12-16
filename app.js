48b9015b38bd8600d8f4137a8f7a2ff691f46936
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-12-16 11:20:53 +0530

    new codes for API:Subhrajit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Server = require('./server.js')
 const port = (process.env.PORT || 8080)
 //const app = Server.app()
 
-const app = Server.app();
-console.log(`Listening at http://localhost:${port}`)
+const app = Server.app()
+app.listen(port)
+console.log(`Listening at URL http://localhost:${port}`)
 
